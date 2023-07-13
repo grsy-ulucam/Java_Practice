78ebd9856d4d433fe47879d39a42621d945bd8e3
grsy-ulucam <emre.yucel48.45@gmail.com>
2023-07-13 16:29:41 +0300

    14.07.2023

---
+++ src/lessons/j25_FinalClass.java
@@ -0,0 +1,10 @@
+package lessons;
+
+public class j25_FinalClass {
+
+    final String name="admin";
+
+    final void name(){
+        System.out.println("final method can't be change");
+    }
+}
---
+++ src/lessons/j26_FinalClassChild.java
@@ -0,0 +1,10 @@
+package lessons;
+
+public class j26_FinalClassChild extends j25_FinalClass{
+
+    public static void main(String[] args) {
+        j26_FinalClassChild txx=new j26_FinalClassChild();
+        txx.name();
+        System.out.println(txx.name);
+    }
+}
