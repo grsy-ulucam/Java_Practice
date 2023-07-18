fa0d509e744f732e819a23a307d504b29ef6f24c
grsy-ulucam <emre.yucel48.45@gmail.com>
2023-07-18 15:12:35 +0300

    18.07.2023

---
+++ src/lessons/j42_LambdaExpression.java
@@ -0,0 +1,56 @@
+package lessons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class j42_LambdaExpression {
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>(Arrays.asList(1, 23, 45, 67, 89, 45, 23, 99));
+        System.out.println(numbers);
+
+        sayı1(numbers);
+        System.out.println("\n   ***   ");
+        sayı2(numbers);
+        System.out.println("\n   ***   ");
+        sayı3(numbers);
+        System.out.println("\n   ***   ");
+        sayı4(numbers);
+
+    }
+
+    private static void sayı4(List<Integer> numbers) {
+        numbers.
+                stream().
+                forEach(j42_LambdaExpression::yazdır);
+    }
+
+    public static void yazdır(int a) {
+        System.out.print(a + " ");
+    }
+
+    private static void sayı3(List<Integer> numbers) {
+        numbers.
+                stream().
+                forEach(System.out::print);
+    }
+
+    private static void sayı2(List<Integer> numbers) {
+        numbers.
+                stream().
+                forEach(t -> System.out.print(t + " "));
+    }
+
+    private static void sayı1(List<Integer> numbers) {
+        for (Integer w : numbers) {
+            System.out.print(w + " ");
+        }
+    }
+     public  static  boolean ciftMi(int X){
+        return  X%2==0;
+     }
+    public  static  boolean tekMi(int X){
+        return  X%2==1;
+    }
+}
---
+++ src/lessons/j43_Filter.java
@@ -0,0 +1,84 @@
+package lessons;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class j43_Filter {
+    public static void main(String[] args) {
+
+        List<Integer>data=new ArrayList<>(Arrays.asList(12,23,45,56,78,99));
+          text1(data);
+        System.out.println("\n  ***");
+          text2(data);
+        System.out.println("\n  ***");
+        text3(data);
+        System.out.println("\n  ***");
+        text4(data);
+        System.out.println("\n  ***");
+        text5(data);
+        System.out.println("\n  ***");
+        text6(data);
+        System.out.println("\n  ***");
+        text7(data);
+        System.out.println("\n  ***");
+        text8(data);
+    }
+
+    private static void text8(List<Integer> data) {
+        data.
+                stream().
+                filter(j42_LambdaExpression::tekMi).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+
+    private static void text7(List<Integer> data) {
+        data.
+                stream().
+                filter(j42_LambdaExpression::ciftMi).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+
+    private static void text6(List<Integer> data) {
+
+        data.
+                stream().
+                filter(t->t%2==0|| t>55).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+
+    private static void text5(List<Integer> data) {
+        data.
+                stream().
+                filter(j42_LambdaExpression::ciftMi).
+                filter(t->t<45).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+
+    private static void text4(List<Integer> data) {
+        data.
+                stream().
+                filter(j42_LambdaExpression::ciftMi).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+    
+    private static void text3(List<Integer> data) {
+        data.
+                stream().
+                filter(t->t%2==0).
+                forEach(j42_LambdaExpression::yazdır);
+    }
+    private static void text2(List<Integer> data) {
+        for (Integer s:data) {
+            if (s%2==0){
+                System.out.print(s+" ");
+            }
+        }
+    }
+    private static void text1(List<Integer>data) {
+        for (Integer w:data) {
+            System.out.print(w+" ");
+        }
+    }
+
+}
